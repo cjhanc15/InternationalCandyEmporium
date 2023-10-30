974b719e577fc6577cc8c313a0ffb570d7c5ff1e
Cybyl Jade <cybyl.hancock@us.af.mil>
2023-10-29 21:35:05 -0600

    CF: started maven project, created home page

---
+++ internationalcandyemporium/src/main/java/com/gcu/internationalcandyemporium/InternationalcandyemporiumApplication.java
@@ -0,0 +1,13 @@
+package com.gcu.internationalcandyemporium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternationalcandyemporiumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternationalcandyemporiumApplication.class, args);
+	}
+
+}
