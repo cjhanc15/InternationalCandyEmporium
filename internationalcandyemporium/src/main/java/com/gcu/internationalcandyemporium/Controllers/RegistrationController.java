cd66cf9bfdae2aadb55a2a038d7898d3eef202a2
DavidAdler1 <123208664+DavidAdler1@users.noreply.github.com>
2023-11-19 10:03:18 -0700

    Update RegistrationController.java

--- internationalcandyemporium/src/main/java/com/gcu/internationalcandyemporium/Controllers/RegistrationController.java
+++ internationalcandyemporium/src/main/java/com/gcu/internationalcandyemporium/Controllers/RegistrationController.java
@@ -1,10 +1,6 @@
 package com.gcu.internationalcandyemporium.Controllers;
 
-import com.gcu.internationalcandyemporium.Models.User;
-import com.gcu.internationalcandyemporium.Service.RegistrationService;
-
-import jakarta.validation.Valid;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -12,11 +8,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import com.gcu.Registration.model.User;
+import com.gcu.Registration.service.RegistrationService;
+
 @Controller
 public class RegistrationController {
-
+    
     private final RegistrationService registrationService;
 
+    @Autowired
     public RegistrationController(RegistrationService registrationService) {
         this.registrationService = registrationService;
     }
@@ -28,9 +28,7 @@ public class RegistrationController {
     }
 
     @PostMapping("/register")
-    public String processRegistration(@ModelAttribute("user") @Valid User user, BindingResult result) {
-        // Validation logic...
-
+    public String processRegistration(@ModelAttribute("user") User user, BindingResult result) {
         if (result.hasErrors()) {
             return "registration";
         }
@@ -41,4 +39,6 @@ public class RegistrationController {
         return "registration-success";
     }
 }
+
+
 
