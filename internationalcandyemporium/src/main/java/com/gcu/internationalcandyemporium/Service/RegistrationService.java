8155a98cddc17345e722bd7c41760fdb04b0cdef
DavidAdler1 <123208664+DavidAdler1@users.noreply.github.com>
2023-11-19 10:06:06 -0700

    Update RegistrationService.java

--- internationalcandyemporium/src/main/java/com/gcu/internationalcandyemporium/Service/RegistrationService.java
+++ internationalcandyemporium/src/main/java/com/gcu/internationalcandyemporium/Service/RegistrationService.java
@@ -2,6 +2,8 @@ package com.gcu.internationalcandyemporium.Service;
 
 import com.gcu.internationalcandyemporium.Models.User;
 
+
 public interface RegistrationService {
     void processRegistration(User user);
 }
+
