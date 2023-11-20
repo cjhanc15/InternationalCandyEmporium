6da78e6944a32a58070d9a85c27e60197c0bb851
Cybyl J Fine <cybyl.hancock@gmail.com>
2023-11-20 13:02:55 -0700

    CF: can successfully add a product to database

--- internationalcandyemporium/src/main/java/com/gcu/internationalcandyemporium/Service/ProductsService.java
+++ internationalcandyemporium/src/main/java/com/gcu/internationalcandyemporium/Service/ProductsService.java
@@ -20,10 +20,6 @@ public class ProductsService implements ProductsServiceInterface{
 
     @Override
     public List<ProductModel> getAllProducts() {
-        productsDataService.create(new ProductModel("Snickers", "Chocolate bar with caramel and peanuts", "https://i.ebayimg.com/images/g/RHkAAOSwdEZgxkh0/s-l1200.webp", 1.99, 100));
-        productsDataService.create(new ProductModel("Buttercup", "Peanut butter and chocolate cup", "buttercup_image_url", 2.49, 80));
-        productsDataService.create(new ProductModel("CandyCorn", "Tri-colored halloween candy", "https://m.media-amazon.com/images/I/71OIhyRTp7L.jpg", 0.99, 150));        
-
         return productsDataService.findAll();
     }
 }
